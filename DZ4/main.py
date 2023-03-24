8bed23b52a65146e99093e759abfb3afdc18adae
Nataly <lalliana@list.ru>
2023-03-24 23:07:11 +0300

    Task 24 added

---
+++ DZ4/main.py
@@ -0,0 +1,12 @@
+n = int(input("Введите количество кустов черники: "))
+arr = list()
+for i in range(n):
+    x = int(input("Введите количество ягод на каждом кусте: "))
+    arr.append(x)
+
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1]+arr[0])
+print(max(arr_count))
+
